5fd9c5f53573d4abf6718c083ee1639aa2f9c3b9
Fadi <fadybd1@gmail.com>
2023-04-15 21:57:36 +0300

    improved styling + search filter

--- src/components/Admin/ItemList/ItemList.jsx
+++ src/components/Admin/ItemList/ItemList.jsx
@@ -1,50 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { getAllItemInstances ,fetchItemNames} from "../../../api/AdminService";
+import { getAllItemInstances, fetchItemNames } from "../../../api/AdminService";
 import { fetchedItemTypes } from "../../../api/WarehouseService";
 import useAdminRole from "../../../hooks/useAdminRole";
 import AdminLayout from "../AdminLayout";
-import {  useTheme } from '@mui/material/styles';
 import { useTranslation } from "react-i18next";
-
-import {
-  Box,
-  TextField,
-  OutlinedInput,
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Chip,
-  Select
-} from "@mui/material";
-
+import { TextField, MenuItem, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import CategoryIcon from "@mui/icons-material/Category";
+import LabelIcon from "@mui/icons-material/Label";
+import SyncIcon from "@mui/icons-material/Sync";
 import "./ItemList.scss";
+import { Grid, Card, CardContent, CardActions, Button } from "@mui/material";
 
+const useItemFilter = (initialItems) => {
+  const [filteredItems, setFilteredItems] = useState(initialItems);
+  const [itemType, setItemType] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [itemState, setItemState] = useState("");
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
+  useEffect(() => {
+    const filtered = initialItems.filter(
+      (item) =>
+        (itemType === "" || itemType === item.itemType.name) &&
+        (itemName === "" || itemName === item.itemName) &&
+        (itemState === "" || itemState === item.state)
+    );
+    setFilteredItems(filtered);
+  }, [initialItems, itemType, itemName, itemState]);
+
+  return {
+    filteredItems,
+    itemType,
+    setItemType,
+    itemName,
+    setItemName,
+    itemState,
+    setItemState,
+  };
 };
 
-
-
-function getStyles(name, TypeName, theme) {
-  return {
-    fontWeight:
-      TypeName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
-
 const ItemList = () => {
-
   useAdminRole();
   const { i18n } = useTranslation();
   const direction = i18n.language === "he" ? "rtl" : "ltr";
@@ -52,6 +47,16 @@ const ItemList = () => {
   const [items, setItems] = useState([]);
   const [type, setType] = useState([]);
   const [names, setName] = useState([]);
+
+  const {
+    filteredItems,
+    itemType,
+    setItemType,
+    itemName,
+    setItemName,
+    itemState,
+    setItemState,
+  } = useItemFilter(items);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -62,52 +67,19 @@ const ItemList = () => {
         console.error("Error fetching warehouse items.", error);
       }
     };
-    const fetchedTypes=async()=>{
-      const  type= await fetchedItemTypes();
+    const fetchedTypes = async () => {
+      const type = await fetchedItemTypes();
       setType(type);
-    }
-    const fetchNames=async()=>{
-      const  names2= await fetchItemNames();
+    };
+    const fetchNames = async () => {
+      const names2 = await fetchItemNames();
       setName(names2);
-    }
+    };
     fetchItems();
     fetchedTypes();
     fetchNames();
-  },[]);
+  }, []);
 
-  
-  const theme = useTheme();
-  const [itemName, setItemName] = useState([]);
-  const [itemType, setItemType] = useState([]);
-  const [itemState, setItemState] = useState([]);
-
-  const handleTypeChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setItemType(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
-    }
-    const handleNameChange = (event) => {
-      const {
-        target: { value },
-      } = event;
-      setItemName(
-        // On autofill we get a stringified value.
-        typeof value === 'string' ? value.split(',') : value,
-      );
-      }
-      const handleStateChange = (event) => {
-        const {
-          target: { value },
-        } = event;
-        setItemState(
-          // On autofill we get a stringified value.
-          typeof value === 'string' ? value.split(',') : value,
-        );
-        }
   return (
     <div className="admin-equipment-list">
       <AdminLayout direction={direction}></AdminLayout>
@@ -115,123 +87,108 @@ const ItemList = () => {
         <p className="title">
           Effortlessly Browse and Filter Equipment Categories
         </p>
-        <div className="search">
-          <TextField
-            id="outlined-basic"
-            variant="outlined"
-            fullWidth
-            label="Search By Category"
-          />
+        <div className="search-filters">
+          <div className="search">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              fullWidth
+              label="Search By Category"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <div className="filters">
+            <TextField
+              select
+              label="Filter by Type"
+              value={itemType}
+              onChange={(event) => setItemType(event.target.value)}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <CategoryIcon />
+                  </InputAdornment>
+                ),
+              }}
+            >
+              {type.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select
+              label="Filter by Name"
+              value={itemName}
+              onChange={(event) => setItemName(event.target.value)}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LabelIcon />
+                  </InputAdornment>
+                ),
+              }}
+            >
+              {names.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select
+              label="Filter by State"
+              value={itemState}
+              onChange={(event) => setItemState(event.target.value)}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SyncIcon />
+                  </InputAdornment>
+                ),
+              }}
+            >
+              {["TAKEN", "DAMAGED", "IN_MAINTENANCE", "AVAILABLE"].map(
+                (option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                )
+              )}
+            </TextField>
+          </div>
         </div>
-        <div>
-      <FormControl sx={{ m: 1, width: 300,fullWidth: true,size:'medium' }}>
-        <InputLabel id="Type-multiple-chip-label">Chip</InputLabel>
-        <Select
-          labelId="Tybe"
-          id="Type-multiple-chip"
-          multiple
-          value={itemType}
-          onChange={handleTypeChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {type.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, itemType, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl sx={{ m: 1, width: 300,fullWidth: true,size:'medium' }}>
-        <InputLabel id="Name-multiple-chip-label">Chip</InputLabel>
-        <Select
-          labelId="name"
-          id="Name-multiple-chip"
-          multiple
-          value={itemName}
-          onChange={handleNameChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, itemName, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl sx={{ m: 1, width: 300,fullWidth: true,size:'medium' }}>
-        <InputLabel id="ItemState-multiple-chip-label">Chip</InputLabel>
-        <Select
-          labelId="ItemState"
-          id="ItemState-multiple-chip"
-          multiple
-          value={itemState}
-          onChange={handleStateChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {['DAMAGED',
-            'IN_MAINTENANCE',
-            'AVAILABLE'].map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, itemState, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      
-
-      
-    </div>
         <div className="equipments-grid">
-          
-        {items && items.filter((item) =>( itemType.length === 0 || itemType.includes(item.itemType.name))  &&(
-        itemName.length === 0 || itemName.includes(item.itemName)) &&(
-          itemState.length === 0 || itemState.includes(item.state))
-        ) 
-          .map((item) => (
-            <div key={item.id} className="list-item">
-              <div className="item-details">
-                <h3>{item.itemName}</h3>
-                <p>{item.state}</p>
-              </div>
-              <div className="item-tag">{item.itemType.name}</div>
-            </div>
-          ))}
+          <Grid container spacing={2}>
+            {filteredItems.map((item) => (
+              <Grid item xs={12} sm={6} md={4} lg={2.5} key={item.id}>
+                <Card className="item-card" elevation={3}>
+                  <CardContent className="item-card__details">
+                    <p className="item-card__title">{item.itemName}</p>
+                    <p className="item-card__body">{item.state}</p>
+                  </CardContent>
+                  <CardActions className="item-card__actions">
+                    <Button className="item-card__button" size="small">
+                      More info
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </main>
     </div>
