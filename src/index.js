a3405a3033e1d07ae46ca209542a8b4367a980b8
Fadi <fadybd1@gmail.com>
2023-04-15 21:48:37 +0300

    translation file split

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 i18n
   .use(HttpApi)
@@ -21,16 +22,38 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: "/translations/{{lng}}.json",
+      loadPath: "/translations/{{lng}}/{{ns}}.json",
     },
+    ns: [
+      "home",
+      "navbar",
+      "login",
+      "warehouse",
+      "itemReservation",
+      "sidebar",
+      "borrowRequests",
+    ],
+    defaultNS: "home",
   });
+
+const theme = createTheme({
+  components: {
+    MuiModal: {
+      defaultProps: {
+        disableScrollLock: true,
+      },
+    },
+  },
+});
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <Router>
     <AuthProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </Router>
 );
