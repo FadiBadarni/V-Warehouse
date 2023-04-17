92261743fb66db63e5c13a5a2bc78bb928bd5235
Fadi <fadybd1@gmail.com>
2023-04-17 16:25:34 +0300

    feat: user update API function

--- src/main/java/com/example/visualvortex/controllers/AdminControllers/ImportUsersController.java
+++ src/main/java/com/example/visualvortex/controllers/AdminControllers/ImportUsersController.java
@@ -2,14 +2,10 @@ package com.example.visualvortex.controllers.AdminControllers;
 
 import com.example.visualvortex.services.User.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.example.visualvortex.services.EmailService;
 
-import javax.mail.MessagingException;
 import java.util.Map;
 
 @RestController
@@ -19,8 +15,9 @@ public class ImportUsersController {
     private final UserService userService;
 
     @PostMapping("/importUsers")
-    public void importUsers(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> importUsers(@RequestParam("file") MultipartFile file) {
         userService.importUsers(file);
+        return ResponseEntity.ok("Users imported successfully");
     }
 
     @PutMapping("/{id}/role")
@@ -30,6 +27,15 @@ public class ImportUsersController {
         return ResponseEntity.ok("User role updated successfully");
     }
 
+    @PutMapping("/update-user/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody Map<String, Object> request) {
+        String email = (String) request.get("email");
+        String username = (String) request.get("username");
+        String role = (String) request.get("role");
+        Integer year = (Integer) request.get("year");
+        userService.updateUser(id, email, username, role, year);
+        return ResponseEntity.ok("User updated successfully");
+    }
 
     @PutMapping("/create-user")
     public   void createUser(@RequestBody Map<String, String> request) {
@@ -39,9 +45,10 @@ public class ImportUsersController {
            userService.createUser(recipient,role,year);
     }
 
-    @PutMapping("delete-user/{id}")
-    public void deleteUser(@PathVariable Long id) {
+    @DeleteMapping("/delete-user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.delete(id);
+        return ResponseEntity.ok("User deleted successfully");
     }
 
 
