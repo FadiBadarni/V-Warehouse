f5ceeef23e86c5ffca3e6ad3d7f25b176070aa37
Fadi <fadybd1@gmail.com>
2023-04-21 19:32:03 +0300

    test: :white_check_mark: Login test
    added test for login functionality.

--- backend/src/test/java/com/example/visualvortex/controllers/LoginControllerTest.java
+++ backend/src/test/java/com/example/visualvortex/controllers/LoginControllerTest.java
@@ -1,11 +1,10 @@
 package com.example.visualvortex.controllers;
 
+import com.example.visualvortex.dtos.UserDTOS.LoginDto;
+import com.example.visualvortex.dtos.UserDTOS.UserDTO;
 import com.example.visualvortex.entities.User.User;
-import com.example.visualvortex.entities.User.UserRole;
 import com.example.visualvortex.services.User.UserService;
-import com.example.visualvortex.services.User.JwtUtil;
-import com.example.visualvortex.repositories.UserRepository;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +12,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -31,42 +35,57 @@ public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private UserService userService;
 
-    @MockBean
-    private JwtUtil jwtUtil;
+    @Test
+    void testLoginSuccess() throws Exception {
+        String username = "testuser";
+        String password = "testpassword";
+        String token = "sample.token.value";
 
-    @MockBean
-    private UserRepository userRepository;
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUsername(username);
+        loginDto.setPassword(password);
 
-//    @Test
-//    void testLogin() throws Exception {
-//        // Define the input and expected output
-//        String inputJson = "{\"username\":\"testuser\", \"password\":\"testpassword\"}";
-//        String expectedToken = "sample_token";
-//        String expectedResponse = "{\"token\":\"sample_token\",\"userInfo\":{\"email\":\"testuser@example.com\",\"username\":\"testuser\",\"year\":2023}}";
-//
-//        // Mock the behavior of userService, jwtUtil, and userRepository
-//        when(userService.authenticateUser("testuser", "testpassword")).thenReturn(true);
-//        when(userService.loadUserByUsername("testuser")).thenReturn(new org.springframework.security.core.userdetails.User("testuser", "testpassword", new ArrayList<>()));
-//        when(jwtUtil.generateToken(any())).thenReturn(expectedToken);
-//
-//        User testUser = User.builder()
-//                .email("testuser@example.com")
-//                .username("testuser")
-//                .year(2023)
-//                .password("testpassword")
-//                .role(UserRole.USER)
-//                .build();
-//
-//        when(userRepository.findByUsername("testuser")).thenReturn(testUser);
-//
-//        // Perform the request and check the response
-//        mockMvc.perform(post("/api/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(inputJson))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(expectedResponse));
-//    }
+        User user = new User();
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(username);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("token", token);
+        response.put("userInfo", userDTO);
+
+        when(userService.authenticateUser(anyString(), anyString())).thenReturn(Optional.of(user));
+        when(userService.generateToken(any(UserDetails.class))).thenReturn(token);
+        when(userService.createUserDTO(user)).thenReturn(userDTO);
+        when(userService.createResponse(anyString(), any(UserDTO.class))).thenReturn(response);
+
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value(token));
+    }
+
+
+    @Test
+    void testLoginUnauthorized() throws Exception {
+        String username = "testuser";
+        String password = "testpassword";
+
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUsername(username);
+        loginDto.setPassword(password);
+
+        when(userService.authenticateUser(anyString(), anyString())).thenReturn(Optional.empty());
+
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isUnauthorized());
+    }
 }
