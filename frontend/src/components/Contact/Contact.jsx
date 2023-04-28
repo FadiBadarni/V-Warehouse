d730fc98a9f39e77c1abf80ba0e5d2e52e41d684
Fadi <fadybd1@gmail.com>
2023-04-28 10:32:33 +0300

    feat: :sparkles: Contact email service

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import { motion } from "framer-motion";
+import emailjs from "emailjs-com";
 import "./Contact.scss";
 
 const Contact = () => {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: { staggerChildren: 0.2 },
-    },
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        event.target,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
-    <motion.Box
-      className="contact"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-    >
-      <Typography variant="h4" className="contact__title">
-        Get in Touch
-      </Typography>
+    <Box className="contact">
       <Box className="contact__content">
+        <Typography variant="h4" className="contact__title">
+          Get in Touch
+        </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
             <Box className="contact__info">
@@ -35,46 +49,58 @@ const Contact = () => {
                 Contact Information
               </Typography>
               <Typography variant="body1" className="contact__info-item">
-                <strong>Address:</strong> 1234 Example St, Anytown, USA
+                <strong>Address:</strong> 1234 Fadi St, Anytown, Israel
               </Typography>
               <Typography variant="body1" className="contact__info-item">
-                <strong>Phone:</strong> (555) 123-4567
+                <strong>Phone:</strong> (052) 418-3083
               </Typography>
               <Typography variant="body1" className="contact__info-item">
-                <strong>Email:</strong> info@example.com
+                <strong>Email:</strong> fadibadarnii@gmail.com
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={6}>
             <Paper className="contact__form-paper">
-              <form className="contact__form">
+              <form className="contact__form" onSubmit={handleSubmit}>
                 <Box className="contact__form-group">
                   <TextField
                     id="name"
+                    name="name"
                     label="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     variant="outlined"
                     fullWidth
                     className="contact__form-field"
+                    required
                   />
                 </Box>
                 <Box className="contact__form-group">
                   <TextField
                     id="email"
+                    name="email"
                     label="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     variant="outlined"
                     fullWidth
                     className="contact__form-field"
+                    required
                   />
                 </Box>
                 <Box className="contact__form-group">
                   <TextField
                     id="message"
+                    name="message"
                     label="Message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                     variant="outlined"
                     multiline
                     rows={4}
                     fullWidth
                     className="contact__form-field"
+                    required
                   />
                 </Box>
                 <Box className="contact__form-group">
@@ -92,7 +118,7 @@ const Contact = () => {
           </Grid>
         </Grid>
       </Box>
-    </motion.Box>
+    </Box>
   );
 };
 
