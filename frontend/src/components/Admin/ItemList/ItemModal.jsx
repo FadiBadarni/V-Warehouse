6db8061368cd3a813a795c33503b48eac31a6707
Fadi <fadybd1@gmail.com>
2023-06-10 18:10:32 +0300

    Dialog update in items list page

--- frontend/src/components/Admin/ItemList/ItemModal.jsx
+++ frontend/src/components/Admin/ItemList/ItemModal.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Modal } from "semantic-ui-react";
-import { MenuItem, Select, Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Typography,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import { updateEquipmentItem } from "../../../api/AdminService";
+
 const ItemModal = ({
   modalOpen,
   toggleModal,
@@ -17,95 +27,127 @@ const ItemModal = ({
         selectedItem.itemType.name
       );
       toggleModal(); // close the modal after successful update
-      // You might want to refresh your list of items here or show a success message
     } catch (error) {
       console.error("Error updating item:", error);
     }
   };
+
   return (
-    <Modal open={modalOpen} onClose={toggleModal}>
-      <div className="item-modal">
-        <div className="item-modal__header">Item Details</div>
-        <div className="item-modal__content">
-          {selectedItem && (
-            <div>
-              <h2>{selectedItem.itemName}</h2>
-              <p>ID: {selectedItem.id}</p>
-              <div className="item-modal__fields">
-                <div className="item-modal__field">
-                  State:{" "}
-                  <Select
-                    value={selectedItem.state}
-                    onChange={(event) =>
-                      setSelectedItem({
-                        ...selectedItem,
-                        state: event.target.value,
-                      })
-                    }
+    <Dialog
+      open={modalOpen}
+      onClose={toggleModal}
+      aria-labelledby="form-dialog-title"
+      className="custom-dialog"
+    >
+      <DialogTitle id="form-dialog-title" className="custom-dialog-title">
+        Item Details
+      </DialogTitle>
+      <DialogContent className="custom-dialog-content">
+        {selectedItem && (
+          <>
+            <Typography variant="h6" className="custom-dialog-itemname">
+              {selectedItem.itemName}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              className="custom-dialog-itemid"
+            >
+              ID: {selectedItem.id}
+            </Typography>
+            <TextField
+              select
+              label="State"
+              value={selectedItem.state}
+              onChange={(event) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  state: event.target.value,
+                })
+              }
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className="custom-dialog-select"
+            >
+              {["TAKEN", "DAMAGED", "IN_MAINTENANCE", "AVAILABLE"].map(
+                (option) => (
+                  <MenuItem
+                    key={option}
+                    value={option}
+                    className="custom-dialog-menu-item"
                   >
-                    {["TAKEN", "DAMAGED", "IN_MAINTENANCE", "AVAILABLE"].map(
-                      (option) => (
-                        <MenuItem key={option} value={option}>
-                          {option}
-                        </MenuItem>
-                      )
-                    )}
-                  </Select>
-                </div>
-                <div className="item-modal__field">
-                  Item Type:{" "}
-                  <Select
-                    value={selectedItem.itemType.name}
-                    onChange={(event) =>
-                      setSelectedItem({
-                        ...selectedItem,
-                        itemType: {
-                          ...selectedItem.itemType,
-                          name: event.target.value,
-                        },
-                      })
-                    }
-                  >
-                    {itemTypes.map((type, index) => (
-                      <MenuItem key={index} value={type}>
-                        {type}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </div>
-              </div>
-              <p>Item Description: {selectedItem.itemDescription}</p>
-              <h3>Attributes:</h3>
-              <ul className="item-modal__attributes">
-                {selectedItem.itemType.attributes.map((attribute) => (
-                  <li key={attribute.id}>
-                    {attribute.attributeName}: {attribute.attributeValue}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className="item-modal__actions">
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={toggleModal}
-            className="item-modal__button"
-          >
-            Close
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            className="item-modal__button"
-          >
-            Update
-          </Button>
-        </div>
-      </div>
-    </Modal>
+                    {option}
+                  </MenuItem>
+                )
+              )}
+            </TextField>
+            <TextField
+              select
+              label="Item Type"
+              value={selectedItem.itemType.name}
+              onChange={(event) =>
+                setSelectedItem({
+                  ...selectedItem,
+                  itemType: {
+                    ...selectedItem.itemType,
+                    name: event.target.value,
+                  },
+                })
+              }
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className="custom-dialog-select"
+            >
+              {itemTypes.map((type, index) => (
+                <MenuItem
+                  key={index}
+                  value={type}
+                  className="custom-dialog-menu-item"
+                >
+                  {type}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <DialogContentText className="custom-dialog-text">
+              Item Description: {selectedItem.itemDescription}
+            </DialogContentText>
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              className="custom-dialog-itemid"
+            >
+              Attributes:
+            </Typography>
+            <ul className="item-modal__attributes">
+              {selectedItem.itemType.attributes.map((attribute) => (
+                <li key={attribute.id} className="custom-dialog-attribute-item">
+                  {attribute.attributeName}: {attribute.attributeValue}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </DialogContent>
+      <DialogActions className="custom-dialog-actions">
+        <Button
+          variant="outlined"
+          onClick={toggleModal}
+          className="custom-dialog-close-button"
+        >
+          Close
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          className="custom-dialog-update-button"
+        >
+          Update
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
